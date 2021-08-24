5913141ea6782c7e2a393534f8564e8e27d98e01
Ashton Gasser <ashtongasser@gmail.com>
2021-08-23 23:24:26 -0500

    compile error styling

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -47,7 +47,14 @@ const App = () => {
         <div id="root"></div>
         <script>
           window.addEventListener('message', (event) => {
+              try{
             eval(event.data);
+              } catch (err) {
+                  const root = document.querySelector('#root');
+                  root.innerHTML ='<div style = "color: red;"><h4>Runtime Error</h4>' + err +
+                  '</div>'
+                  console.error(err);
+              }
           }, false);
         </script>
       </body>
