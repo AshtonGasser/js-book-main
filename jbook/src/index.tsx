ec0f1ee3ac9593581e2d9b6e732f68178f0c533a
Ashton Gasser <ashtongasser@gmail.com>
2021-08-23 21:54:43 -0500

    css code feature added

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+
 const App = () => {
   const ref = useRef<any>();
   const [input, setInput] = useState('');
@@ -11,7 +12,7 @@ const App = () => {
   const startService = async () => {
     ref.current = await esbuild.startService({
       worker: true,
-      wasmURL: '/esbuild.wasm',
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   };
   useEffect(() => {
@@ -37,6 +38,11 @@ const App = () => {
     // console.log(result);
 
     setCode(result.outputFiles[0].text);
+    try {
+      eval(result.outputFiles[0].text);
+    } catch (err) {
+        //sweetalert
+    }
   };
 
   return (
