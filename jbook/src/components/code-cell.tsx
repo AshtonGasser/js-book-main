1ceaaf5adf883904e37049bf29f541604819422b
Ashton Gasser <ashtongasser@gmail.com>
2021-08-30 13:16:22 -0500

    orgress bar created, css added comments cleaned up

--- jbook/src/components/code-cell.tsx
+++ jbook/src/components/code-cell.tsx
@@ -5,6 +5,7 @@ import Resizable from './resizable';
 import { Cell } from '../Redux';
 import { useActions } from '../hooks/use-actions';
 import { useTypedSelector } from '../hooks/use-typed-selector';
+import './code-cell.css'
 
 interface CodeCellProps {
   cell: Cell;
@@ -15,6 +16,10 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 750);
@@ -22,10 +27,11 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   return (
-    <Resizable direction="vertical">
+    <Resizable direction='vertical'>
       <div
         style={{
           height: 'calc(100% - 10px)',
@@ -33,13 +39,21 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
           flexDirection: 'row',
         }}
       >
-        <Resizable direction="horizontal">
+        <Resizable direction='horizontal'>
           <CodeEditor
             initialValue={cell.content}
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        {!bundle || bundle.loading ? (
+          <div className = "progress-cover">
+          <progress className = "progress is -small is-primary" max = "100">
+            Loading...
+            </progress>
+            </div>
+        ) : (
+          <Preview code={bundle.code} err={bundle.err} />
+        )}
       </div>
     </Resizable>
   );
