02b46949dfa1bf7d1e1bf90c31aa2e8214f6d2e2
Ashton Gasser <ashtongasser@gmail.com>
2021-08-30 21:32:01 -0500

    bug hunting

--- jbook/src/bundler/index.ts
+++ jbook/src/bundler/index.ts
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 
+
 let service: esbuild.Service;
 const bundle = async (rawCode: string) => {
   if (!service) {
@@ -29,10 +30,10 @@ const bundle = async (rawCode: string) => {
       code: result.outputFiles[0].text,
       err: '',
     };
-  } catch (err) {
+  } catch (err: any) {
     return {
       code: '',
-      err: err.message,
+      err: err.message ?? '',
     };
   }
 };
